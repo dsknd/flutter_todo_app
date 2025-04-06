355953613412abf651eeafb251bbfabd68c16f8d
dsknd <daisukeendo@outlook.jp>
2025-04-06 23:41:45 +0900

    Firestore保存するロジックを変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter/material.dart';
-import 'dart:convert'; // 追加！
-import 'package:shared_preferences/shared_preferences.dart'; // 追加！flutter pub get
+// import 'dart:convert'; // 追加！
+// import 'package:shared_preferences/shared_preferences.dart'; // 追加！flutter pub get
 
-void main() {
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // flutterfire configure で自動生成されたやつ
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -39,21 +47,42 @@ class _TodoAppState extends State<TodoApp> {
       }
     }
 
+    // void _saveTodos() async {
+    //   final prefs = await SharedPreferences.getInstance();
+    //   final todoJson = jsonEncode(_todos);
+    //   await prefs.setString('todo_list', todoJson);
+    // }
+
     void _saveTodos() async {
-      final prefs = await SharedPreferences.getInstance();
-      final todoJson = jsonEncode(_todos);
-      await prefs.setString('todo_list', todoJson);
+      final docRef = FirebaseFirestore.instance.collection('todos').doc('user1'); // ユーザーIDは仮
+      await docRef.set({'items': _todos});
     }
 
+    // void _loadTodos() async {
+    //   final prefs = await SharedPreferences.getInstance();
+    //   final todoJson = prefs.getString('todo_list');
+    //   if (todoJson != null) {
+    //     final List<dynamic> decoded = jsonDecode(todoJson);
+    //     setState(() {
+    //       _todos.clear();
+    //       _todos.addAll(decoded.cast<String>());
+    //     });
+    //   }
+    // }
+
     void _loadTodos() async {
-      final prefs = await SharedPreferences.getInstance();
-      final todoJson = prefs.getString('todo_list');
-      if (todoJson != null) {
-        final List<dynamic> decoded = jsonDecode(todoJson);
-        setState(() {
-          _todos.clear();
-          _todos.addAll(decoded.cast<String>());
-        });
+      final docRef = FirebaseFirestore.instance.collection('todos').doc('user1');
+      final doc = await docRef.get();
+
+      if (doc.exists) {
+        final data = doc.data();
+        final items = data?['items'];
+        if (items is List) {
+          setState(() {
+            _todos.clear();
+            _todos.addAll(List<String>.from(items));
+          });
+        }
       }
     }
     
